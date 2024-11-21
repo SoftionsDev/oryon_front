af67e373a17153e6cbc75b1c3b30bd0187e9651c
sebastian rocha <jsebas791@gmail.com>
2024-11-21 10:37:43 -0500

    fix: dependencies

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://ec2-3-136-25-237.us-east-2.compute.amazonaws.com:8001"
+export const API_URL = "http://ec2-3-136-25-237.us-east-2.compute.amazonaws.com"
 
 export const ROLES = {
     Admin: 1,
